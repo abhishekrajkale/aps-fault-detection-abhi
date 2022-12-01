41bec83383d4726cba47b8c0b0d68709ebdfd180
abhishekrajkale <abhikaleak8@gmail.com>
2022-12-01 18:29:36 +0530

    updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="sensor",
     version="0.0.2",
     author="ineuron",
-    author_email="avnish@ineuron.ai",
+    author_email="abhikaleak8@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements(),
 )
