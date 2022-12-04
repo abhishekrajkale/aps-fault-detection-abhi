47dc6d5ffafe7a5343cb5e65936aeccc608813f6
abhishekrajkale <abhikaleak8@gmail.com>
2022-12-04 16:28:57 +0530

    updated

--- sensor/__init__.py
+++ sensor/__init__.py
@@ -1,3 +1,3 @@
-from dotenv import lode_dotenv
+from dotenv import load_dotenv
 print(f"Loding environment varible from .env file")
 load_dotenv()
--- sensor/utils/__init__.py
+++ sensor/utils/__init__.py
@@ -5,12 +5,21 @@ from sensor.config import mongo_client
 import os,sys 
 
 def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
+    """
+    Description: This function return collection as dataframe
+    =========================================================
+    Params:
+    database_name: database name
+    collection_name: collection name
+    =========================================================
+    return Pandas dataframe of a collection
+    """
     try:
         logging.info(f"Reding data from database: {database_name} and collection: {collection_name} ")
         df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
-        loggin.info(f"Found columns:{df.columns}")
+        logging.info(f"Found columns:{df.columns}")
         if "_id" in df.columns:
-            loggin.info(f"Droping column: _id ")
+            logging.info(f"Droping column: _id ")
             df = df.drop("_id",axis=1)
         logging.info(f"Row and columns in df: {df.shape}")
         return df 
